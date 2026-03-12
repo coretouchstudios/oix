1200eebb5b8a3c14ee24ba421ea571957380d889
coretouchstudios <coretouchstudios@gmail.com>
2026-03-12 22:03:31 +0100

    fix: OIX monorepo config

--- apps/web-platform/next.config.cjs
+++ apps/web-platform/next.config.cjs
@@ -1,16 +1,9 @@
-/** @type {import('next').NextConfig} */
-
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true
   },
-
   eslint: {
     ignoreDuringBuilds: true
-  },
-
-  experimental: {
-    optimizePackageImports: ["react", "react-dom"]
   }
 }
 
